4b27523d95348b235f20a74a656f025a2f1952e8
jairocaro1012 <61439558+jairocaro1012@users.noreply.github.com>
2020-11-02 16:50:47 -0500

    Add files via upload

---
+++ SA2020IIG01_PROY02_3/codes/proyecto.sql
@@ -0,0 +1,26 @@
+create database segimiento;
+use segimiento;
+
+create table  total(
+fecha varchar(100)  primary key,
+Casos varchar(30),
+Recuperados varchar(30),
+Hospitalizados varchar(30),
+Unidades varchar(30),
+Muertes varchar(30)
+);
+create table localidades(
+fecha varchar(100),
+nombre varchar(30),
+casos varchar(30)
+);
+create table depa(
+fecha varchar(100),
+nombre varchar(30),
+casos varchar(30)
+);
+
+
+Select * from localidades;
+Select * from total;
+Select * from depa;
